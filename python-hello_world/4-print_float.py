a8462570684bfc280187b14cd6c3a66cf708e12d
KoltonR <gamerlife467@gmail.com>
2023-05-08 10:11:30 -0700

    4-print_float.py

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 number = 3.14159
-float_number = float(number)
-print(f"Float: {float_number}")
+
+# use an f-string to print the float with 2 digits of precision
+print(f"Float: {number:.2f}\n")
+
