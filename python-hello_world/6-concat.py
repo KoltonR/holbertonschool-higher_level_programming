f084c147e66fb3fcd48296e92b0dd29e9af64053
KoltonR <gamerlife467@gmail.com>
2023-05-08 10:37:24 -0700

    6-concat.py

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+str1 = "Welcome to"
+str2 = "Holberton School"
+# concatenate the two strings with a space in between using the + operator
+# then print the resulting string to the console
+print(str1 + " " + str2 + "!")
+
