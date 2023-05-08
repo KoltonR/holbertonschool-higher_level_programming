04d1b89fb39c44c8c5d958b65a1a709fd59bd645
KoltonR <gamerlife467@gmail.com>
2023-05-08 11:20:04 -0700

    6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 str1 = "Welcome to"
-str2 = "Battery Street"
+str2 = "Holberton School"
 print(str1 + " " + str2 + "!")
+str3 = "Welcome to"
+str4 = "Battery Street"
+print(str3 + " " + str4 + "!')
 
