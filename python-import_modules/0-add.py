d970796b6bb142dd56e53ab58b1020304f5fcd4e
KoltonR <gamerlife467@gmail.com>
2023-05-09 07:05:51 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+# 0_add.py
 
 if __name__ == "__main__":
-    """
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-     Prints the result of the addition between two numbers
-
-     """
-     a = 1
-     b = 2
-     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
