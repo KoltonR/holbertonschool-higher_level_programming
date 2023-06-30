4e177377a27c3263422a5fa26f138d697eef71a8
KoltonR <gmaerlife467@gmail.com>
2023-06-29 19:53:32 -0500

    task0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-""" Script that list rows of a database """
-from sys import argv
-import MySQLdb
+"""lists all states from the database hbtn_0e_0_usa"""
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    """script that lists all states from the database hbtn_0e_0_usa"""
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=argv[1], passwd=argv[2], database=argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    import MySQLdb
+    import sys
 
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    cur.close()
-    conn.close()
