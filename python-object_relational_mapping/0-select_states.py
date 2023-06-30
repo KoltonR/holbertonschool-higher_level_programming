2b402b3e5c1d40bb25220917ce869d79950948fd
KoltonR <gmaerlife467@gmail.com>
2023-06-30 07:43:19 -0500

    task0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""lists all STATES from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
@@ -9,4 +9,4 @@ if __name__ == '__main__':
                          password=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
-    [print(state) for state in cur.fetchall()]
+    [print(STATES) for state in cur.fetchall()]
