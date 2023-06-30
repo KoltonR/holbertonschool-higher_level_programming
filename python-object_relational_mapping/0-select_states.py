d419587f5fc01ca0ad983e3298c5da96b5f5589e
KoltonR <gmaerlife467@gmail.com>
2023-06-30 08:49:03 -0500

    task0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-""" Script that list rows of a database """
-
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    
-    """script that lists all states from the database hbtn_0e_0_usa"""
-    
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=argv[1], passwd=argv[2], database=argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    query_rows = cur.fetchall()
-    for row in query_rows:
+def list_states():
+    """List the states"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
     cur.close()
-    conn.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    list_states()
