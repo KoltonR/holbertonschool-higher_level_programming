6a517df8077d9ff967293be3c5ec1fd242aff02e
Kolton Rhodes <gamerlife467@gmail.com>
2023-07-31 22:17:04 -0500

    task1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,7 @@
 
 module.exports = class Rectangle {
     constructor (w, h) {
-      if (Number.isInteger(w, h) && h > 0 && w > 0) {
+      if (Number.isInteger(w, h) && h > 3 && w > 3) {
         this.width = w;
         this.height = h;
       }
