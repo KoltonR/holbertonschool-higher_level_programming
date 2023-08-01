be64930095df0a23fdd2db04c7b4e4c3a34d9d36
Kolton Rhodes <gamerlife467@gmail.com>
2023-07-31 21:52:01 -0500

    fix3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
+const process = require('process');
+const args = process.argv;
 
-if (arg) {
-  console.log(arg);
-} else {
+if (!args[2]) {
   console.log('No argument');
+} else {
+  console.log(args[2]);
 }
